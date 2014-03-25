807de3176182c3066f995cb1d8c6459ef690df24
Nick Moss <nickm@neu.local>
2014-03-24 20:02:12 -0600

    Start of NProc/NProcTask.

---
+++ lib/src/NProcTask.cpp
@@ -0,0 +1,91 @@
+/*
+ 
+      ___           ___           ___     
+     /\__\         /\  \         /\__\    
+    /::|  |       /::\  \       /:/  /    
+   /:|:|  |      /:/\:\  \     /:/  /     
+  /:/|:|  |__   /::\~\:\  \   /:/  /  ___ 
+ /:/ |:| /\__\ /:/\:\ \:\__\ /:/__/  /\__\
+ \/__|:|/:/  / \:\~\:\ \/__/ \:\  \ /:/  /
+     |:/:/  /   \:\ \:\__\    \:\  /:/  / 
+     |::/  /     \:\ \/__/     \:\/:/  /  
+     /:/  /       \:\__\        \::/  /   
+     \/__/         \/__/         \/__/    
+ 
+ 
+Neu, Copyright (c) 2013-2014, Andrometa LLC
+All rights reserved.
+
+neu@andrometa.net
+http://neu.andrometa.net
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+ 
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+ 
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+ 
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+ 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+*/
+
+#include <neu/NProcTask.h>
+
+using namespace std;
+using namespace neu;
+
+namespace neu{
+  
+  class NProc;
+  
+  class NProcTask_{
+  public:
+    NProcTask_(NProcTask* o)
+    : o_(o){
+      
+    }
+    
+    ~NProcTask_(){
+      
+    }
+    
+    void queue(NProc* proc, nvar& r, double priority){
+      
+    }
+    
+  private:
+    NProcTask* o_;
+  };
+  
+} // end namespace neu
+
+NProcTask::NProcTask(){
+  x_ = new NProcTask_(this);
+}
+
+NProcTask::~NProcTask(){
+  delete x_;
+}
+
+void NProcTask::queue(NProc* proc, nvar& r, double priority){
+  x_->queue(proc, r, priority);
+}
