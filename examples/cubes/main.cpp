ba252616b3ed0849da403485a1221ffedb727d80
Nick Moss <nickm@neu.local>
2014-04-17 09:46:44 -0600

    Added examples.

---
+++ examples/cubes/main.cpp
@@ -0,0 +1,294 @@
+#include <iostream>
+
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+#include <neu/NProgram.h>
+#include <neu/NPQueue.h>
+#include <neu/NPLParser.h>
+#include <neu/NPLVector.h>
+#include <neu/NPLObject.h>
+#include <neu/NPLModule.h>
+#include <neu/NRandom.h>
+#include <neu/NSys.h>
+
+using namespace std;
+using namespace neu;
+using namespace npl;
+
+static const size_t N = 512*10;
+static const float SIZE = 0.05;
+static const size_t WINDOW_WIDTH = 1024;
+static const size_t WINDOW_HEIGHT = 1024;
+static const float MIN_R = 0.01;
+static const float MAX_R = 0.10;
+static const float MAX_V = 0.50;
+static const float MAX_SPIN = 30.0;
+static const float REPEL = -2e-1;
+static const float SLEEP = 0.001;
+static const size_t RESET_INTERVAL = 250;
+
+static GLuint cubeDL;
+static NRandom rng;
+
+class Cube : public NPLObject{
+public:
+  Cube(){
+    
+  }
+
+  void reset(const float3& u){
+    this->u = u;
+   
+    v[0] = rng.uniform(-MAX_V, MAX_V);
+    v[1] = rng.uniform(-MAX_V, MAX_V);
+    v[2] = rng.uniform(-MAX_V, MAX_V);
+
+    a = {0.0, 0.0, 0.0};
+
+    av[0] = rng.uniform(-MAX_SPIN, MAX_SPIN);
+    av[1] = rng.uniform(-MAX_SPIN, MAX_SPIN);
+    av[2] = rng.uniform(-MAX_SPIN, MAX_SPIN);
+    
+    r = rng.uniform(MIN_R, MAX_R);
+  }
+
+  float3 u;
+  float3 v;
+  float3 a;
+  float3 av;
+  float r;
+};
+
+typedef NVector<Cube*> CubeVec;
+CubeVec cubeVec;
+
+static NPQueue queue(8);
+size_t t = 0;
+
+NPLModule module;
+
+void animate(){
+  if(t % RESET_INTERVAL == 0){
+    float3 u = {float(rng.uniform(-20, 20)),
+                float(rng.uniform(0, 50)),
+                float(rng.uniform(-50, 10))};
+
+    for(size_t i = 0; i < cubeVec.size(); ++i){
+      Cube* cube = cubeVec[i];
+      cube->reset(u);
+    }
+  }
+
+  queue.run();
+
+  glutPostRedisplay();
+  ++t;
+}
+
+void createCube(){
+  cubeDL = glGenLists(1);
+
+  glNewList(cubeDL, GL_COMPILE);
+
+  static GLfloat v0[] = {-1, -1, 1};
+  static GLfloat v1[] = {1, -1, 1};
+  static GLfloat v2[] = {1, 1, 1};
+  static GLfloat v3[] = {-1, 1, 1};
+  static GLfloat v4[] = {-1, -1, -1};
+  static GLfloat v5[] = {1, -1, -1};
+  static GLfloat v6[] = {1, 1, -1};
+  static GLfloat v7[] = {-1, 1, -1};
+  static GLubyte red[]    = {255, 0, 0, 255};
+  static GLubyte green[]  = {0, 255, 0, 255};
+  static GLubyte blue[]   = {0, 0, 255, 255};
+  static GLubyte white[]  = {255, 255, 255, 255};
+  static GLubyte yellow[] = {0, 255, 255, 255};
+  static GLubyte black[]  = {0, 0, 0, 255};
+  static GLubyte orange[] = {255, 255, 0, 255};
+  static GLubyte purple[] = {255, 0, 255, 0};
+
+  glBegin(GL_TRIANGLES);
+
+  glColor4ubv(red);
+  glVertex3fv(v0);
+  glColor4ubv(green);
+  glVertex3fv(v1);
+  glColor4ubv(blue);
+  glVertex3fv(v2);
+  
+  glColor4ubv(red);
+  glVertex3fv(v0);
+  glColor4ubv(blue);
+  glVertex3fv(v2);
+  glColor4ubv(white);
+  glVertex3fv(v3);
+  
+  glColor4ubv(green);
+  glVertex3fv(v1);
+  glColor4ubv(black);
+  glVertex3fv(v5);
+  glColor4ubv(orange);
+  glVertex3fv(v6);
+  
+  glColor4ubv(green);
+  glVertex3fv(v1);
+  glColor4ubv(orange);
+  glVertex3fv(v6);
+  glColor4ubv(blue);
+  glVertex3fv(v2);
+  
+  glColor4ubv(black);
+  glVertex3fv(v5);
+  glColor4ubv(yellow);
+  glVertex3fv(v4);
+  glColor4ubv(purple);
+  glVertex3fv(v7);
+  
+  glColor4ubv(black);
+  glVertex3fv(v5);
+  glColor4ubv(purple);
+  glVertex3fv(v7);
+  glColor4ubv(orange);
+  glVertex3fv(v6);
+   
+  glColor4ubv(yellow);
+  glVertex3fv(v4);
+  glColor4ubv(red);
+  glVertex3fv(v0);
+  glColor4ubv(white);
+  glVertex3fv(v3);
+  
+  glColor4ubv(yellow);
+  glVertex3fv(v4);
+  glColor4ubv(white);
+  glVertex3fv(v3);
+  glColor4ubv(purple);
+  glVertex3fv(v7);
+  
+  glColor4ubv(white);
+  glVertex3fv(v3);
+  glColor4ubv(blue);
+  glVertex3fv(v2);
+  glColor4ubv(orange);
+  glVertex3fv(v6);
+  
+  glColor4ubv(white);
+  glVertex3fv(v3);
+  glColor4ubv(orange);
+  glVertex3fv(v6);
+  glColor4ubv(purple);
+  glVertex3fv(v7);
+  
+  glColor4ubv(green);
+  glVertex3fv(v1);
+  glColor4ubv(red);
+  glVertex3fv(v0);
+  glColor4ubv(yellow);
+  glVertex3fv(v4);
+  
+  glColor4ubv(green);
+  glVertex3fv(v1);
+  glColor4ubv(yellow);
+  glVertex3fv(v4);
+  glColor4ubv(black);
+  glVertex3fv(v5);
+  
+  glEnd();
+
+  glEndList();
+}
+
+void drawCube(float size,
+	      const float3& u,
+              const float3& a){
+  
+  glPushMatrix();
+
+  glTranslatef(u[0], u[1], u[2]);
+  glScalef(size, size, size);
+
+  glRotatef(a[0], 1.0, 0.0, 0.0);
+  glRotatef(a[1], 0.0, 1.0, 0.0);
+  glRotatef(a[2], 0.0, 0.0, 1.0);
+
+  glCallList(cubeDL);
+
+  glPopMatrix();
+}
+
+void display(){
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  gluLookAt(0.0, 5.0, 25.0,
+	    0.0, 10.0, 0.0,
+	    0.0, 1.0, 0.0);
+
+  for(CubeVec::iterator itr = cubeVec.begin(),
+	itrEnd = cubeVec.end(); itr != itrEnd; ++itr){
+    Cube* cube = *itr;
+    drawCube(cube->r, cube->u, cube->a);
+  }
+  
+  NSys::sleep(SLEEP);
+  glutSwapBuffers();
+}
+void initGL(void){
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  
+  gluPerspective(60.0,
+                 static_cast<float>(WINDOW_WIDTH)/WINDOW_HEIGHT, 0.1, 1024); 
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  
+  glEnable(GL_DEPTH_TEST);
+
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+  glFrontFace(GL_CCW);
+  glClearColor(0, 0, 0, 0);
+
+  createCube();
+}
+
+int main(int argc, char** argv){
+  NProgram program(argc, argv);
+
+  glutInit(&argc, argv);
+
+  NPLParser parser;
+  nvar code = parser.parseFile("kernel.npl");
+
+  cout << "code is: " << code << endl;
+  
+  if(!module.compile(code)){
+    cerr << "failed to compile" << endl;
+    return 1;
+  }
+
+  for(size_t i = 0; i < N; ++i){
+    Cube* c = new Cube;
+    cubeVec.push_back(c);
+
+    NPLFunc* f = new NPLFunc;
+    module.getFunc({"Cube", "run", 0}, f);
+    f->o = c;
+
+    queue.add(f);
+  }
+
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+  glutCreateWindow("Cubes");
+  initGL();
+  glutDisplayFunc(display);
+  glutIdleFunc(animate);
+  glutMainLoop(); 
+
+  return 0;
+}
---
+++ examples/waves/main.cpp
@@ -0,0 +1,282 @@
+#include <iostream>
+
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+#include <neu/NProgram.h>
+#include <neu/NPQueue.h>
+#include <neu/NPLParser.h>
+#include <neu/NPLVector.h>
+#include <neu/NPLObject.h>
+#include <neu/NPLModule.h>
+#include <neu/NSys.h>
+
+using namespace std;
+using namespace neu;
+using namespace npl;
+
+static const size_t WINDOW_WIDTH = 1024;
+static const size_t WINDOW_HEIGHT = 1024;
+
+static const float WIDTH = 35;
+static const float HEIGHT = 35;
+static const float ROWS = 256;
+static const float COLUMNS = 256;
+static const float HEIGHT_MULTIPLIER = 2;
+
+static NPQueue runQueue(8);
+static NPQueue interactQueue(8);
+
+inline void glVertex(const float3& u){
+  glVertex3f(u[0], u[1], u[2]);
+}
+
+inline void toRGB(const float3& hsv, float3& rgb){
+  static int i;
+  static float f, p, q, t, h;
+  
+  if(hsv[1] == 0){
+    rgb[0] = rgb[1] = rgb[2] = hsv[2];
+    return;
+  }
+
+  h = hsv[0] / 60;
+
+  i = h;
+  f = h - i;
+  p = hsv[2] * (1 - hsv[1]);
+  q = hsv[2] * (1 - hsv[1] * f);
+  t = hsv[2] * (1 - hsv[1] * (1 - f));
+
+  switch(i){
+    case 0:
+      rgb[0] = hsv[2];
+      rgb[1] = t;
+      rgb[2] = p;
+      break;
+    case 1:
+      rgb[0] = q;
+      rgb[1] = hsv[2];
+      rgb[2] = p;
+      break;
+    case 2:
+      rgb[0] = p;
+      rgb[1] = hsv[2];
+      rgb[2] = t;
+      break;
+    case 3:
+      rgb[0] = p;
+      rgb[1] = q;
+      rgb[2] = hsv[2];
+      break;
+    case 4:
+      rgb[0] = t;
+      rgb[1] = p;
+      rgb[2] = hsv[2];
+      break;
+    default:
+      rgb[0] = hsv[2];
+      rgb[1] = p;
+      rgb[2] = q;
+      break;
+  }
+}
+
+class Cell : public NPLObject{
+public:
+  Cell()
+    : h(0),
+      s(0){
+    
+  }
+  
+  double h;
+  double s;
+};
+
+typedef NVector<Cell*> CellVec;
+
+CellVec cellVec;
+
+void initCells(){
+  for(size_t i = 0; i < ROWS; ++i){
+    for(size_t j = 0; j < COLUMNS; ++j){
+      cellVec.push_back(new Cell);
+    }
+  }
+}
+
+inline Cell* getCell(int i, int j){
+  if(i >= ROWS){
+    return 0;
+  }
+  else if(i < 0){
+    return 0;
+  }
+  
+  if(j >= COLUMNS){
+    return 0;
+  }
+  else if(j < 0){
+    return 0;
+  }
+
+  return cellVec[i + j * ROWS];
+}
+
+NPLModule module;
+
+class InteractFunc : public NPLFunc{
+public:
+  Cell* c;
+};
+
+void addNeighbor(Cell* c1, Cell* c2){
+  if(c1 && c2){
+    InteractFunc* f = new InteractFunc;
+    module.getFunc({"Cell", "interact", 1}, f);
+    f->o = c1;
+    f->c = c2;
+    interactQueue.add(f);
+  }
+}
+
+void initBehavior(){
+  for(size_t i = 0; i < ROWS; ++i){
+    for(size_t j = 0; j < COLUMNS; ++j){
+      Cell* c = getCell(i, j);
+
+      NPLFunc* f = new NPLFunc;
+      module.getFunc({"Cell", "run", 0}, f);
+      f->o = c;
+
+      runQueue.add(f);
+
+      addNeighbor(c, getCell(i - 1, j));
+      addNeighbor(c, getCell(i + 1, j));
+      addNeighbor(c, getCell(i, j - 1));
+      addNeighbor(c, getCell(i, j + 1));
+      addNeighbor(c, getCell(i - 1, j - 1));
+      addNeighbor(c, getCell(i + 1, j + 1));
+      addNeighbor(c, getCell(i - 1, j + 1));
+      addNeighbor(c, getCell(i + 1, j - 1));
+    }
+  }
+}
+
+void drawCells(){
+  glMatrixMode(GL_MODELVIEW);
+
+  static float3 v1;
+  static float3 v2;
+  static float3 rgb;
+  static float3 hsv = {1, 1, 1};
+  static const float cellWidth = WIDTH/(COLUMNS - 1);
+  static const float cellHeight = HEIGHT/(ROWS - 1);
+  static const float left = -cellWidth*(COLUMNS - 1)/2;
+  static const float top = -cellHeight*(ROWS - 1)/2;
+  
+  for(size_t i = 0; i < COLUMNS - 1; i++){
+    glBegin(GL_TRIANGLE_STRIP);
+
+    for(size_t j = 0; j < ROWS; j++){
+
+      Cell* c1 = getCell(i, j);
+      Cell* c2 = getCell(i + 1, j);
+      
+      hsv[0] = c1->h*360;
+      toRGB(hsv, rgb);
+
+      glColor4f(rgb[0], rgb[1], rgb[2], 1);
+
+      v1[0] = left + i * cellWidth;
+      v1[1] = top + j * cellHeight;
+      v1[2] = c1->h * HEIGHT_MULTIPLIER;
+
+      v2[0] = left + (i + 1) * cellWidth;
+      v2[1] = top + j * cellHeight;
+      v2[2] = c2->h * HEIGHT_MULTIPLIER;
+        
+      glVertex3f(v1[0], v1[1], v1[2]);
+      glVertex3f(v2[0], v2[1], v2[2]);
+    }
+
+    glEnd();
+  }
+}
+
+void animate(){
+  runQueue.run();
+  interactQueue.run();
+
+  glutPostRedisplay();
+}
+
+void display(){
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  gluLookAt(0.0, 0.0, 50.0,
+	    0.0, 0.0, 0.0,
+	    0.0, 1.0, 0.0);
+
+  glRotatef(-70.0, 1.0, 0.0, 0.0);
+
+  drawCells();
+
+  NSys::sleep(0.0001);
+
+  glutSwapBuffers();
+}
+
+void initGL(void){
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  
+  gluPerspective(60.0,
+                 static_cast<float>(WINDOW_WIDTH)/WINDOW_HEIGHT, 0.1, 1024); 
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_BLEND);
+  
+  glShadeModel(GL_SMOOTH);
+
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+  glFrontFace(GL_CCW);
+  glClearColor(0.5, 0.5, 0.5, 1);
+}
+
+int main(int argc, char** argv){
+  NProgram program(argc, argv);
+
+  glutInit(&argc, argv);
+
+  NPLParser parser;
+  nvar code = parser.parseFile("kernel.npl");
+
+  cout << "code is: " << code << endl;
+  
+  if(!module.compile(code)){
+    cerr << "failed to compile" << endl;
+    return 1;
+  }
+
+  initCells();
+  initBehavior();
+
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+  glutCreateWindow("Waves");
+  initGL();
+  glutDisplayFunc(display);
+  glutIdleFunc(animate);
+  glutMainLoop();  
+
+  return 0;
+}
