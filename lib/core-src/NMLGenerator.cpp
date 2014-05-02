4c8db2d3eb249da6651c121aaeced2fb96800997
Nick Moss <nickm@neu.local>
2014-05-02 16:28:12 -0600

    Progress on NMLGenerator.

--- lib/core-src/NMLGenerator.cpp
+++ lib/core-src/NMLGenerator.cpp
@@ -95,7 +95,83 @@ namespace{
     FKEY_Pow_2,
     FKEY_Set_2,
     FKEY_Block_n,
-    FKEY_Call_2
+    FKEY_Call_2,
+    FKEY_ScopedBlock_n,
+    FKEY_Ret_0,
+    FKEY_Ret_1,
+    FKEY_Break_0,
+    FKEY_Continue_0,
+    FKEY_If_2,
+    FKEY_If_3,
+    FKEY_While_2,
+    FKEY_For_4,
+    FKEY_New_1,
+    FKEY_Class_1,
+    FKEY_Idx_2,
+    FKEY_Dot_2,
+    FKEY_DotPut_2,
+    FKEY_Cs_1,
+    FKEY_Call_1,
+    FKEY_Def_2,
+    FKEY_PushBack_2,
+    FKEY_TouchMultimap_1,
+    FKEY_TouchList_1,
+    FKEY_Keys_1,
+    FKEY_PushFront_2,
+    FKEY_PopBack_1,
+    FKEY_HasKey_2,
+    FKEY_Insert_3,
+    FKEY_Clear_1,
+    FKEY_Empty_1,
+    FKEY_Back_1,
+    FKEY_Erase_2,
+    FKEY_Merge_2,
+    FKEY_OuterMerge_2,
+    FKEY_Switch_3,
+    FKEY_IsFalse_1,
+    FKEY_IsTrue_1,
+    FKEY_IsDefined_1,
+    FKEY_IsString_1,
+    FKEY_IsSymbol_1,
+    FKEY_IsFunction_1,
+    FKEY_IsSymbolic_1,
+    FKEY_IsNumeric_1,
+    FKEY_IsReference_1,
+    FKEY_IsPointer_1,
+    FKEY_IsInteger_1,
+    FKEY_IsRational_1,
+    FKEY_IsReal_1,
+    FKEY_GetStr_1,
+    FKEY_GetVec_1,
+    FKEY_GetList_1,
+    FKEY_GetAnySequence_1,
+    FKEY_GetMap_1,
+    FKEY_GetMultimap_1,
+    FKEY_GetAnyMap_1,
+    FKEY_Append_2,
+    FKEY_Normalize_1,
+    FKEY_Head_1,
+    FKEY_SetHead_2,
+    FKEY_ClearHead_1,
+    FKEY_NumKeys_1,
+    FKEY_Size_1,
+    FKEY_MapEmpty_1,
+    FKEY_AllEmpty_1,
+    FKEY_HasVector_1,
+    FKEY_HasList_1,
+    FKEY_HasMap_1,
+    FKEY_HasMultimap_1,
+    FKEY_PopFront_1,
+    FKEY_AllKeys_1,
+    FKEY_TouchVector_1,
+    FKEY_TouchMap_1,
+    FKEY_Open_2,
+    FKEY_Save_2,
+    FKEY_Unite_2,
+    FKEY_Unite_3,
+    FKEY_Intersect_2,
+    FKEY_Intersect_3,
+    FKEY_Complement_2
   };
   
   typedef NMap<nstr, SymbolKey> SymbolMap;
@@ -145,6 +221,82 @@ namespace{
     _functionMap[{"Block", -1}] = FKEY_Block_n;
     _functionMap[{"Call", 2}] = FKEY_Call_2;
     _functionMap[{"Pow", 2}] = FKEY_Pow_2;
+    _functionMap[{"ScopedBlock", -1}] = FKEY_ScopedBlock_n;
+    _functionMap[{"Ret", 0}] = FKEY_Ret_0;
+    _functionMap[{"Ret", 1}] = FKEY_Ret_1;
+    _functionMap[{"Break", 0}] = FKEY_Break_0;
+    _functionMap[{"Continue", 0}] = FKEY_Continue_0;
+    _functionMap[{"If", 2}] = FKEY_If_2;
+    _functionMap[{"If", 3}] = FKEY_If_3;
+    _functionMap[{"While", 2}] = FKEY_While_2;
+    _functionMap[{"For", 4}] = FKEY_For_4;
+    _functionMap[{"New", 1}] = FKEY_New_1;
+    _functionMap[{"Class", 1}] = FKEY_Class_1;
+    _functionMap[{"Idx", 2}] = FKEY_Idx_2;
+    _functionMap[{"Dot", 2}] = FKEY_Dot_2;
+    _functionMap[{"DotPut", 2}] = FKEY_DotPut_2;
+    _functionMap[{"Cs", 1}] = FKEY_Cs_1;
+    _functionMap[{"Call", 1}] = FKEY_Call_1;
+    _functionMap[{"Def", 2}] = FKEY_Def_2;
+    _functionMap[{"PushBack", 2}] = FKEY_PushBack_2;
+    _functionMap[{"TouchMultimap", 1}] = FKEY_TouchMultimap_1;
+    _functionMap[{"TouchList", 1}] = FKEY_TouchList_1;
+    _functionMap[{"Keys", 1}] = FKEY_Keys_1;
+    _functionMap[{"PushFront", 2}] = FKEY_PushFront_2;
+    _functionMap[{"PopBack", 1}] = FKEY_PopBack_1;
+    _functionMap[{"HasKey", 2}] = FKEY_HasKey_2;
+    _functionMap[{"Insert", 3}] = FKEY_Insert_3;
+    _functionMap[{"Clear", 1}] = FKEY_Clear_1;
+    _functionMap[{"Empty", 1}] = FKEY_Empty_1;
+    _functionMap[{"Back", 1}] = FKEY_Back_1;
+    _functionMap[{"Erase", 2}] = FKEY_Erase_2;
+    _functionMap[{"Merge", 2}] = FKEY_Merge_2;
+    _functionMap[{"OuterMerge", 2}] = FKEY_OuterMerge_2;
+    _functionMap[{"Switch", 3}] = FKEY_Switch_3;
+    _functionMap[{"IsFalse", 1}] = FKEY_IsFalse_1;
+    _functionMap[{"IsTrue", 1}] = FKEY_IsTrue_1;
+    _functionMap[{"IsDefined", 1}] = FKEY_IsDefined_1;
+    _functionMap[{"IsString", 1}] = FKEY_IsString_1;
+    _functionMap[{"IsSymbol", 1}] = FKEY_IsSymbol_1;
+    _functionMap[{"IsFunction", 1}] = FKEY_IsFunction_1;
+    _functionMap[{"IsSymbolic", 1}] = FKEY_IsSymbolic_1;
+    _functionMap[{"IsNumeric", 1}] = FKEY_IsNumeric_1;
+    _functionMap[{"IsReference", 1}] = FKEY_IsReference_1;
+    _functionMap[{"IsPointer", 1}] = FKEY_IsPointer_1;
+    _functionMap[{"IsInteger", 1}] = FKEY_IsInteger_1;
+    _functionMap[{"IsRational", 1}] = FKEY_IsRational_1;
+    _functionMap[{"IsReal", 1}] = FKEY_IsReal_1;
+    _functionMap[{"GetStr", 1}] = FKEY_GetStr_1;
+    _functionMap[{"GetVec", 1}] = FKEY_GetVec_1;
+    _functionMap[{"GetList", 1}] = FKEY_GetList_1;
+    _functionMap[{"GetAnySequence", 1}] = FKEY_GetAnySequence_1;
+    _functionMap[{"GetMap", 1}] = FKEY_GetMap_1;
+    _functionMap[{"GetMultimap", 1}] = FKEY_GetMultimap_1;
+    _functionMap[{"GetAnyMap", 1}] = FKEY_GetAnyMap_1;
+    _functionMap[{"Append", 2}] = FKEY_Append_2;
+    _functionMap[{"Normalize", 1}] = FKEY_Normalize_1;
+    _functionMap[{"Head", 1}] = FKEY_Head_1;
+    _functionMap[{"SetHead", 2}] = FKEY_SetHead_2;
+    _functionMap[{"ClearHead", 1}] = FKEY_ClearHead_1;
+    _functionMap[{"NumKeys", 1}] = FKEY_NumKeys_1;
+    _functionMap[{"Size", 1}] = FKEY_Size_1;
+    _functionMap[{"MapEmpty", 1}] = FKEY_MapEmpty_1;
+    _functionMap[{"AllEmpty", 1}] = FKEY_AllEmpty_1;
+    _functionMap[{"HasVector", 1}] = FKEY_HasVector_1;
+    _functionMap[{"HasList", 1}] = FKEY_HasList_1;
+    _functionMap[{"HasMap", 1}] = FKEY_HasMap_1;
+    _functionMap[{"HasMultimap", 1}] = FKEY_HasMultimap_1;
+    _functionMap[{"PopFront", 1}] = FKEY_PopFront_1;
+    _functionMap[{"AllKeys", 1}] = FKEY_AllKeys_1;
+    _functionMap[{"TouchVector", 1}] = FKEY_TouchVector_1;
+    _functionMap[{"TouchMap", 1}] = FKEY_TouchMap_1;
+    _functionMap[{"Open", 2}] = FKEY_Open_2;
+    _functionMap[{"Save", 2}] = FKEY_Save_2;
+    _functionMap[{"Unite", 2}] = FKEY_Unite_2;
+    _functionMap[{"Unite", 3}] = FKEY_Unite_3;
+    _functionMap[{"Intersect", 2}] = FKEY_Intersect_2;
+    _functionMap[{"Intersect", 3}] = FKEY_Intersect_3;
+    _functionMap[{"Complement", 1}] = FKEY_Complement_2;
   };
   
   class _FunctionMapLoader{
@@ -192,37 +344,112 @@ namespace neu{
     }
     
     void emitStatement(ostream& ostr,
-                       const nvar& v,
+                       const nvar& n,
                        const nstr& indent){
-      if(!v.isFunction()){
-        emitExpression(ostr, v, indent);
+      if(!n.isFunction()){
+        emitExpression(ostr, n, indent);
         return;
       }
       
-      FunctionKey key = getFunctionKey(v);
+      FunctionKey key = getFunctionKey(n);
       
       switch(key){
+        case FKEY_ScopedBlock_n:
         case FKEY_Block_n:{
-          for(size_t i = 0; i < v.size(); ++i){
-            emitStatement(ostr, v[i], indent);
+          for(size_t i = 0; i < n.size(); ++i){
+            emitStatement(ostr, n[i], indent);
           }
           break;
         }
         case FKEY_Var_3:
         case FKEY_Var_2:{
-          emitExpression(ostr, v[0], indent);
+          emitExpression(ostr, n[0], indent);
           ostr << " = ";
-          emitExpression(ostr, v[1], indent);
+          emitExpression(ostr, n[1], indent);
           ostr << ";" << endl;
           break;
         }
+        case FKEY_Ret_0:
+          ostr << indent << "return;" << endl;
+          break;
+        case FKEY_Ret_1:
+          ostr << indent << "return ";
+          emitExpression(ostr, n[0]);
+          break;
+        case FKEY_Break_0:
+          ostr << indent << "break;" << endl;
+          break;
+        case FKEY_Continue_0:
+          ostr << indent << "continue;" << endl;
+          break;
+        case FKEY_If_2:
+        case FKEY_If_3:
+          emitIf(ostr, n, indent);
+          break;
+        case FKEY_While_2:
+          ostr << indent << "while(";
+          emitExpression(ostr, n[0], indent);
+          ostr << "){" << endl;
+          emitStatement(ostr, n[1], indent + "  ");
+          ostr << indent << "}" << endl;
+          break;
+        case FKEY_For_4:
+          ostr << indent << "for(";
+          emitExpression(ostr, n[0], indent);
+          ostr << "; ";
+          emitExpression(ostr, n[1], indent);
+          ostr << "; ";
+          emitExpression(ostr, n[2], indent);
+          ostr << "){" << endl;
+          emitStatement(ostr, n[3], indent + "  ");
+          ostr << indent << "}" << endl;
+          break;
+        case FKEY_Class_1:
+        case FKEY_Switch_3:
         default:
         {
           ostr << indent;
-          emitExpression(ostr, v, indent);
+          emitExpression(ostr, n, indent);
           ostr << ";" << endl;
           break;
         }
+      }
+    }
+    
+    void emitIf(ostream& ostr,
+                const nvar& n,
+                const nstr& indent,
+                bool nested=false){
+      
+      if(n.size() == 3){
+        if(!nested){
+          ostr << indent;
+        }
+        ostr << "if(";
+        emitExpression(ostr, n[0]);
+        ostr << "){" << endl;
+        emitStatement(ostr, n[1], indent + "  ");
+        ostr << indent << "}" << endl;
+        const nvar& e = n[2];
+        if(e.isFunction("If")){
+          ostr << indent << "else ";
+          emitIf(ostr, e, indent, true);
+        }
+        else{
+          ostr << indent << "else{" << endl;
+          emitStatement(ostr, e, indent + "  ");
+          ostr << indent << "}" << endl;
+        }
+      }
+      else if(n.size() == 2){
+        if(!nested){
+          ostr << indent;
+        }
+        ostr << "if(";
+        emitExpression(ostr, n[0], indent);
+        ostr << "){" << endl;
+        emitStatement(ostr, n[1], indent + "  ");
+        ostr << indent << "}" << endl;
       }
     }
     
@@ -283,6 +510,43 @@ namespace neu{
       ostr << op;
     }
     
+    void emitVarBuiltin(ostream& ostr,
+                        const nvar& n,
+                        const nstr& name="",
+                        const nstr& indent=""){
+      emitExpression(ostr, n[0], indent);
+      if(name.empty()){
+        ostr << n.str().lowercase();
+      }
+      else{
+        ostr << name;
+      }
+      
+      ostr << "(";
+      size_t size = n.size();
+      for(size_t i = 1; i < size; ++i){
+        if(i > 1){
+          ostr << ", ";
+        }
+        emitExpression(ostr, n[i]);
+      }
+      ostr << ")";
+    }
+    
+    void emitGenericFunc(ostream& ostr,
+                         const nvar& n){
+
+      ostr << n.str() << "(";
+      size_t size = n.size();
+      for(size_t i = 1; i < size; ++i){
+        if(i > 0){
+          ostr << ", ";
+        }
+        emitExpression(ostr, n[i]);
+      }
+      ostr << ")";
+    }
+    
     void emitExpression(ostream& ostr,
                         const nvar& n,
                         const nstr& indent="",
@@ -299,7 +563,109 @@ namespace neu{
       
       switch(key){
         case FKEY_NO_KEY:
-          NERROR("invalid function: " + n);
+          emitGenericFunc(ostr, n);
+        case FKEY_PushBack_2:
+        case FKEY_TouchMultimap_1:
+        case FKEY_TouchList_1:
+        case FKEY_Keys_1:
+        case FKEY_PushFront_2:
+        case FKEY_PopBack_1:
+        case FKEY_HasKey_2:
+        case FKEY_Insert_3:
+        case FKEY_Clear_1:
+        case FKEY_Empty_1:
+        case FKEY_Back_1:
+        case FKEY_Erase_2:
+        case FKEY_Merge_2:
+        case FKEY_OuterMerge_2:
+        case FKEY_IsFalse_1:
+        case FKEY_IsTrue_1:
+        case FKEY_IsDefined_1:
+        case FKEY_IsString_1:
+        case FKEY_IsSymbol_1:
+        case FKEY_IsFunction_1:
+        case FKEY_IsSymbolic_1:
+        case FKEY_IsNumeric_1:
+        case FKEY_IsReference_1:
+        case FKEY_IsPointer_1:
+        case FKEY_IsInteger_1:
+        case FKEY_IsRational_1:
+        case FKEY_IsReal_1:
+        case FKEY_Append_2:
+        case FKEY_Normalize_1:
+        case FKEY_Head_1:
+        case FKEY_SetHead_2:
+        case FKEY_ClearHead_1:
+        case FKEY_NumKeys_1:
+        case FKEY_Size_1:
+        case FKEY_MapEmpty_1:
+        case FKEY_AllEmpty_1:
+        case FKEY_HasVector_1:
+        case FKEY_HasList_1:
+        case FKEY_HasMap_1:
+        case FKEY_HasMultimap_1:
+        case FKEY_PopFront_1:
+        case FKEY_AllKeys_1:
+        case FKEY_TouchVector_1:
+        case FKEY_TouchMap_1:
+        case FKEY_Open_2:
+        case FKEY_Save_2:
+        case FKEY_Unite_2:
+        case FKEY_Unite_3:
+        case FKEY_Intersect_2:
+        case FKEY_Intersect_3:
+        case FKEY_Complement_2:
+          emitVarBuiltin(ostr, n);
+          break;
+        case FKEY_GetStr_1:
+          emitVarBuiltin(ostr, n, "str");
+          break;
+        case FKEY_GetVec_1:
+          emitVarBuiltin(ostr, n, "vec");
+          break;
+        case FKEY_GetList_1:
+          emitVarBuiltin(ostr, n, "list");
+          break;
+        case FKEY_GetAnySequence_1:
+          emitVarBuiltin(ostr, n, "anySequence");
+          break;
+        case FKEY_GetMap_1:
+          emitVarBuiltin(ostr, n, "map");
+          break;
+        case FKEY_GetMultimap_1:
+          emitVarBuiltin(ostr, n, "multimap");
+          break;
+        case FKEY_GetAnyMap_1:
+          emitVarBuiltin(ostr, n, "anyMap");
+          break;
+        case FKEY_New_1:
+          ostr << "new ";
+          emitExpression(ostr, n[0]);
+          break;
+        case FKEY_Idx_2:
+          emitExpression(ostr, n[0]);
+          ostr << "[";
+          emitExpression(ostr, n[1]);
+          ostr << "]";
+          break;
+        case FKEY_DotPut_2:
+        case FKEY_Dot_2:
+          emitExpression(ostr, n[0]);
+          ostr << ".";
+          emitExpression(ostr, n[1]);
+          break;
+        case FKEY_Cs_1:
+          ostr << "{";
+          emitExpression(ostr, n[0]);
+          ostr << "}";
+          break;
+        case FKEY_Call_1:
+          emitGenericFunc(ostr, n[0]);
+          break;
+        case FKEY_Def_2:
+          // ndm - implement
+          assert(false);
+          break;
         case FKEY_Add_2:
           emitBinOp(ostr, n, " + ", prec);
           break;
